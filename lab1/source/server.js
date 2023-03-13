ca491db5b218328b7341abe02afcf3adcb7676ec
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-13 15:14:27 +0100

    Poprawa tagu video

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     let html = "<h1>Hello World Player</h1>"
     if(req.query.videoFile) {
-        html += `<video <video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
+        html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
     }
     if(req.query.audioFile) {
         html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
