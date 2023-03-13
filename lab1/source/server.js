1ed8affdce1674ca369ceb69b2850e77eb253b1e
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-13 15:13:08 +0100

    Zmiana src

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -5,10 +5,10 @@ const app = express()
 app.get('/', (req, res) => {
     let html = "<h1>Hello World Player</h1>"
     if(req.query.videoFile) {
-        html += `<video id="videoPlayer" controls><source src="${req.query.videoFile}"></video><BR><BR>`
+        html += `<video <video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
     }
     if(req.query.audioFile) {
-        html += `<audio id="audioPlayer" controls><source src="${req.query.audioFile}""></audio><BR><BR>`
+        html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
     }
     res.send(html)
 })
