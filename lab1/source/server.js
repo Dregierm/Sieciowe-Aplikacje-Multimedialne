fa479a55968b0234241ae875a9ebcc1093f8504a
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-13 14:45:33 +0100

    Hello World Player

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let html = "<h1>Hello World Player</h1>"
+    res.send(html)
 })
 
 app.listen(4080)
