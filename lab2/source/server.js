14e635afdff23b0ab820b1d4bd16103350312f28
Mateusz Dregier <dregierm@gmail.com>
2023-03-27 14:48:15 +0200

    button video

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     let html = "<h1>Hello World Player</h1>"
     if(req.query.videoFile) {
         html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
+        html += `<button id="videoCancel" onClick="cancelVideo()">cancel video</button><BR><BR>`
     }
     if(req.query.audioFile) {
         html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
