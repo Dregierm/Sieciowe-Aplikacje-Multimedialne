781446353caaea0870ab3b7e72fc2582481e1c44
Mateusz Dregier <dregierm@gmail.com>
2023-03-27 14:40:56 +0200

    lab1 server

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let html = "<h1>Hello World Player</h1>"
+    if(req.query.videoFile) {
+        html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
+    }
+    if(req.query.audioFile) {
+        html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
+    }
+    res.send(html)
 })
 
 app.listen(4080)
