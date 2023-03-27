e2225b305b863772d6752bf4d7ec335bf6966782
Mateusz Dregier <dregierm@gmail.com>
2023-03-27 14:55:20 +0200

    Dodanie funkcji cancel

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -4,10 +4,12 @@ const app = express()
 
 app.get('/', (req, res) => {
     let html = "<body><h1>Hello World Player</h1>"
+
     if(req.query.videoFile) {
         html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
         html += `<button id="videoCancel" onClick="cancelVideo()">cancel video</button><BR><BR>`
     }
+
     if(req.query.audioFile) {
         html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
         html += `<button id="audioCancel" onClick="cancelAudio()">cancel audio</button><BR><BR>`
@@ -17,6 +19,17 @@ app.get('/', (req, res) => {
         html += `<img id="posterImage" src=${req.query.imgFile}><BR><BR>`
     }
 
+    html += `<script>
+        function cancelVideo() {
+            document.getElementById("videoPlayer").children[0].src="cancel.mp4";
+        }
+
+        function cancelAudio() {
+            document.getElementById("audioPlayer").children[0].src="cancel.mp3";
+        }
+
+    </script>
+`
     html +="</body>"
     res.send(html)
 })
