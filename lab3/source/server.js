d3405baa8603220988fe1bded8c6d9d32b92f779
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-04-17 14:59:30 +0200

    Poprawa nazw

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -20,18 +20,18 @@ app.get('/', (req, res) => {
     if(req.query.videoFile) {
         html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
         html += `<button id="videoCancel" onClick="cancelVideo()">cancel video</button><BR><BR>`
-        html += `<button id="videoAdd" onClick="addToTable('video')">Add video</button><BR><BR>`;
+        html += `<button id="videoAdd" onClick="addToTable('Video')">Add video</button><BR><BR>`;
     }
 
     if(req.query.audioFile) {
         html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
         html += `<button id="audioCancel" onClick="cancelAudio()">cancel audio</button><BR><BR>`
-        html += `<button id="audioAdd" onClick="addToTable('audio')">Add audio</button><BR><BR>`;
+        html += `<button id="audioAdd" onClick="addToTable('Audio')">Add audio</button><BR><BR>`;
     }
 
     if(req.query.imgFile) {
         html += `<img id="posterImage" src=${req.query.imgFile}><BR><BR>`
-        html += `<button id="imgAdd" onClick="addToTable('image')">Add image</button><BR><BR>`;
+        html += `<button id="imgAdd" onClick="addToTable('Image')">Add image</button><BR><BR>`;
 
     }
 
@@ -48,11 +48,11 @@ app.get('/', (req, res) => {
     let counter = 1;
     function addToTable(type) {
         let src;
-        if (type == 'audio')
+        if (type == 'Audio')
             src = document.getElementById('audioPlayer').getElementsByTagName("source")[0].src;
-        else if (type == 'video')
+        else if (type == 'Video')
             src = document.getElementById('videoPlayer').getElementsByTagName("source")[0].src;
-        else if (type == 'image')
+        else if (type == 'Image')
             src = document.getElementById('posterImage').getAttribute('src');
 
         let table = document.getElementById('playlist_table');
