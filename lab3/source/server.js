3cc0d4f903267a8e9f2d973417253ca1b6b883ea
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-04-17 14:29:32 +0200

    lab2

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -3,7 +3,35 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let html = "<body><h1>Hello World Player</h1>"
+
+    if(req.query.videoFile) {
+        html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
+        html += `<button id="videoCancel" onClick="cancelVideo()">cancel video</button><BR><BR>`
+    }
+
+    if(req.query.audioFile) {
+        html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
+        html += `<button id="audioCancel" onClick="cancelAudio()">cancel audio</button><BR><BR>`
+    }
+
+    if(req.query.imgFile) {
+        html += `<img id="posterImage" src=${req.query.imgFile}><BR><BR>`
+    }
+
+    html += `<script>
+        function cancelVideo() {
+            document.getElementById("videoPlayer").src="cancel.mp4";
+        }
+
+        function cancelAudio() {
+            document.getElementById("audioPlayer").src="cancel.mp3";
+        }
+
+    </script>
+`
+    html +="</body>"
+    res.send(html)
 })
 
 app.listen(4080)
